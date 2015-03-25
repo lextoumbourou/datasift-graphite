26e070e6fc5cb49ccdc721b763f07d4d46edb8d0
Lex Toumbourou <lextoumbourou@gmail.com>
2015-03-25 23:09:13 +1100

    Oh yeah, need to actually export a module.

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ from setuptools import setup
 
 setup(
     name='datasift-graphite',
-    version='1.1',
+    version='1.2.0',
     description='Graph your Datasift account metrics in Graphite.',
     author='Lex Toumbourou',
     author_email='lextoumbourou@gmail.com',
     url='https://github.com/lextoumbourou/datasift-graphite',
     license='MIT License',
+    py_modules = ['datasift_graphite'],
     install_requires=['datasift'],
     entry_points={
         'console_scripts': ['datasift-graphite = datasift_graphite:main'],

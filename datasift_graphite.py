6f11d91741fcce3bdcc8d3cb66e12062b96bc233
Lex Toumbourou <lextoumbourou@gmail.com>
2015-03-25 22:44:43 +1100

    Added setup scripts.

--- datasift_graphite.py
+++ datasift_graphite.py
@@ -171,8 +171,10 @@ def get_args():
     return parser.parse_args()
 
 
-def main(args):
+def main():
     """Main loop."""
+    args = get_args()
+
     if args.debug:
         level = logging.DEBUG
     else:
@@ -221,5 +223,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-    args = get_args()
-    main(args)
+    main()
